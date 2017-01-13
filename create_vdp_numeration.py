ce670c00d6d6c915252e136225bdd086ffcd402b
Martti Lepmets <mlepmets@gmail.com>
2017-01-13 16:30:43 +0200

    Update create_vdp_numeration.py
    Add isnumeric() check

--- create_vdp_numeration.py
+++ create_vdp_numeration.py
@@ -1,14 +1,16 @@
 """create_vdp_numeration.py - create .csv file to be used with
 Adobe InDesign "Data Merge" feature."""
 
+import sys
+
 try:
     file_name = input('File name?: ')
     csvfile = open(file_name, 'w')
 
     csvfile.write('number')
 
-    """For duplex print jobs create an additional column in the csv file
-    called "dummy" which will later be assigned to a page without numeration."""
+    """For duplex print jobs create an additional column in the csv file called
+    "dummy" which will later be assigned to a page without numeration."""
     two_sides = input('Duplex? (y/n) ')
     if two_sides.lower() == 'y':
         csvfile.write(',dummy')
@@ -19,9 +21,11 @@ try:
     end = int(input('End of seq.: '))
 
     # loop from start to end number, write to file
-    for i in range(start, end + 1):
-        csvfile.write(str(i) + '\n')
-        
+    if start.isnumeric() and end.isnumeric():
+        for i in range(start, end + 1):
+            csvfile.write(str(i) + '\n')
+    else:
+        sys.exit('Must enter numbers for sequence')
     csvfile.close()
 
 except IOError as e:
